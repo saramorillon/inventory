c4b0156f6f30e521673578e17c32b01db2b72c88
Sara Morillon <sara.morillon.pro@gmail.com>
2022-09-11 20:17:49 +0200

    Remove socket service

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,6 @@
 import cors from 'cors'
 import express, { json } from 'express'
 import session from 'express-session'
-import { createServer } from 'http'
 import passport from 'passport'
 import { Strategy } from 'passport-local'
 import { appLogger } from './libs/logger'
@@ -9,7 +8,6 @@ import { deserializeUser, localStrategy, serializeUser } from './libs/passport'
 import { logger } from './middleware/logger'
 import { router } from './router'
 import { settings } from './settings'
-import { SocketService } from './socket/socket'
 
 passport.serializeUser(serializeUser)
 passport.deserializeUser(deserializeUser)
@@ -25,9 +23,6 @@ app.use(passport.session())
 app.use(logger)
 app.use('/api', router)
 
-const server = createServer(app)
-SocketService.init(server)
-
-server.listen(settings.port, () => {
+app.listen(settings.port, () => {
   appLogger.info('app_start', { port: settings.port })
 })
