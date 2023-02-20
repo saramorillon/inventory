307bb15e07e8e94cb2327a2eaf9f3bdc5edcfae7
Sara Morillon <sara.morillon.pro@gmail.com>
2023-02-20 20:42:52 +0100

    Add push script

---
+++ push.ps1
@@ -0,0 +1,8 @@
+$json = Get-Content back/package.json | ConvertFrom-Json
+$version = Select-Object -InputObject ${json} -ExpandProperty "version"
+$name = Select-Object -InputObject ${json} -ExpandProperty "name"
+echo "Pushing ${name} v${version}"
+docker build -t saramorillon/${name} .
+docker image tag saramorillon/${name} saramorillon/${name}:${version}
+docker push -a saramorillon/${name}
+echo "Done"
