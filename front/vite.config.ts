bd64176e204a8861ffd6c51707e9b18b62818bab
Sara Morillon <sara.morillon@gmail.com>
2024-07-28 11:25:20 +0200

    Remove front vitest config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: { port: 4000, proxy: { '/api': 'http://localhost:3000' } },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    clearMocks: true,
+    mockReset: true,
+    restoreMocks: true,
+    include: ['tests/**/*.test.ts*'],
+    setupFiles: ['tests/setup.tsx'],
+  },
 })
